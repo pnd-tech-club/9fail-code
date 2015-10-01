94310841f6d350d635ffa712f48ef012ed1bf481
Sean Hinchee <seanh@localhost>
2015-10-01 08:27:26 -0500

    Fixing some typos, unused variables, and incorrect formatting

--- compwords.c
+++ compwords.c
@@ -6,18 +6,18 @@ main(int argc, char *argv[])
 {
 	char *buf1[64*1024+1];
 	char *buf2[64*1024+1];
-	int a, b, bsize1, bsize2;
+	long bsize1, bsize2;
 
 	print("Word 1: ");
 	bsize1 = read(0, buf1, sizeof(buf1)-1);
 	print("Word 2: ");
 	bsize2 = read(0, buf2, sizeof(buf2)-1);
 
-	print("Word 1 size: %d bytes\n", bsize1);
-	print("Word 2 size: %d bytes\n", bsize2);
+	print("Word 1 size: %ld bytes\n", bsize1);
+	print("Word 2 size: %ld bytes\n", bsize2);
 	if((memcmp(buf1, buf2, bsize1)) == 0 )
 	{
-		print("The same word!\n");
+		print("The same size word!\n");
 	}
 	else if((memcmp(buf1, buf2, bsize1)) == 1)
 	{
